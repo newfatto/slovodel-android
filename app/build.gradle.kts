53e6df36e4ed8d155acae0e499aa5d134dcf962b
Ekaterina Chernyshova <newfatto@gmail.com>
2026-06-17 17:18:06 +0300

    Fix Android SDK version in CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "ru.slovodel.scorekeeper"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ru.slovodel.scorekeeper"
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
